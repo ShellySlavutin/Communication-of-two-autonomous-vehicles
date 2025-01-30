eadfa59990f9f03ab43cb56bba8649435b5a10ee
Aleksandra Elarinova <sashaelarinova@gmail.com>
2025-01-30 11:51:50 +0200

    code for mp3 player

---
+++ ניסויים לפרוייקט/ניסוי 8 MP3/Mp3Player/Mp3Player.ino
@@ -0,0 +1,17 @@
+#include "DFRobotDFPlayerMini.h" // library
+
+HardwareSerial mp3Serial(1);  //  Defines UART1 for communicating with DFPlayer Mini
+DFRobotDFPlayerMini mp3;      // Create an object to control mp3
+
+void setup()
+{
+  // Start Serial communication with DFPlayer Mini
+  mp3Serial.begin(9600, SERIAL_8N1, 16, 17);  //rx=16, tx=17
+
+  mp3.begin(mp3Serial); // Initialize DFPlayer Mini
+  mp3.volume(30); // Set volume to 30
+
+  mp3.play(1);// Play the first MP3 file (0001.mp3)
+}
+
+void loop() { }
